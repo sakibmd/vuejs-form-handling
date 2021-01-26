ee2ab559c7bc08412dea5c01162ea3638d3f63ee
sakibmd <sakibmd.cse@gmail.com>
2021-01-27 02:49:58 +0600

    practicing component communication (props, emit)

---
+++ vue-mini-projects-for-practicing-purpose/vue-cli-component-communication-with-product-item/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container-fluid">
+    <Navbar />
+    <Items :items="items" @newItemAdded="newItem" />
+  </div>
+</template>
+
+<script>
+import Navbar from "./components/Navbar";
+import Items from "./components/Items";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      items: [
+        { name: "Apple", price: 130 },
+        { name: "Banana", price: 16 },
+        { name: "Orange", price: 90 },
+        { name: "Pineapple", price: 40 },
+      ],
+    };
+  },
+  components: {
+    Navbar,
+    Items,
+  },
+  methods: {
+    newItem(item){
+      console.log(item);
+      this.items.push(item);
+    }
+  }
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+</style>
---
+++ vue-mini-projects-for-practicing-purpose/vue-cli-component-communication-with-product-item/src/components/Items.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="row">
+      
+      <div class="col-md-4 p-2  card" v-for="(item, index) in items" :key="index">
+          <h5>Name: {{ item.name }}</h5>
+          <h5>Price: ${{ item.price }}</h5>
+      </div>
+      <hr>
+      <div class="my-4 card m-auto" style="width:50%">
+          <form @submit.prevent="addNewItem">
+              <h2>Add New Item</h2>
+              <input type="text" v-model="name" class="form-control" placeholder="enter name">
+              <input type="number" v-model="price" class="form-control my-2" placeholder="enter price">
+              <button type="submit" class="btn btn-success my-2">Add</button>
+          </form>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            name: '',
+            price: ''
+        }
+    },
+    props: ['items'],
+    methods:{
+        addNewItem(){
+            var newItem = { name: this.name, price: this.price };
+            this.name = '';
+            this.price = '';
+            this.$emit('newItemAdded', newItem);
+        }
+    }
+}
+</script>
+
+<style>
+    
+</style>
